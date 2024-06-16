a918c7065db4b86b5c77c108460edf021a897582
Valeria Molina <valeriamolina123@gmail.com>
2024-06-15 23:52:34 -0400

    changed workspace settings

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

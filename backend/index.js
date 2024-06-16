67329ca2d62fc201d104ee31f615b5f58fbbe63b
Valeria Molina <valeriamolina123@gmail.com>
2024-06-15 23:48:49 -0400

    integrated prettier and eslint

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;

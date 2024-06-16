76d47134721872b3ab37bce4fcb1beb51f463fa1
Valeria Molina <valeriamolina123@gmail.com>
2024-06-15 23:21:42 -0400

    initial server setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

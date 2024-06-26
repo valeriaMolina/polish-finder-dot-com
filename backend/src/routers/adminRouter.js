cbf8788da594cc51d5e15c665f463d72ef0e565a
Valeria Molina <valeriamolina123@gmail.com>
2024-06-25 22:30:30 -0400

    admins can now revoke roles

--- backend/src/routers/adminRouter.js
+++ backend/src/routers/adminRouter.js
@@ -50,4 +50,41 @@ router.post(
     }
 );
 
+router.post(
+    '/role-revoke',
+    authenticateToken,
+    authorize(permissions.MANAGE_ROLES),
+    async (req, res) => {
+        const { username, roleName } = req.body;
+        logger.info(
+            `Received request to revoke role ${roleName} from user ${username}`
+        );
+        try {
+            // check that user exists
+            const user = await userService.getUserByUsername(username);
+            if (!user) {
+                return res.status(400).json({ error: 'User not found' });
+            }
+            // ensure role exists
+            const role = await roleService.findRolesByName(roleName);
+            if (!role) {
+                return res.status(400).json({ error: 'Role not found' });
+            }
+            // revoke role from user
+            await userRolesService.revokeRoleFromUser(
+                user.user_id,
+                role.role_id
+            );
+            res.status(200).json({
+                message: `Role ${roleName} revoked from user ${username}`,
+            });
+        } catch (err) {
+            res.status(500).json({
+                error: 'Internal Server Error',
+                msg: err.message,
+            });
+        }
+    }
+);
+
 module.exports = router;
--- backend/src/services/userRolesService.js
+++ backend/src/services/userRolesService.js
@@ -21,7 +21,27 @@ async function assignRoleToUser(userId, roleId) {
     return newRoleAssignment;
 }
 
+async function revokeRoleFromUser(userId, roleId) {
+    // assuming user and role exists
+    // sanity check: does the user have the role?
+    try {
+        const userRole = await userRolesModel.findOne({
+            where: { user_id: userId, role_id: roleId },
+        });
+        if (!userRole) {
+            throw new Error('User does not have the role');
+        }
+        // delete the role assignment
+        await userRolesModel.destroy({
+            where: { user_id: userId, role_id: roleId },
+        });
+    } catch (err) {
+        throw new Error(err.message);
+    }
+}
+
 module.exports = {
     findUserRolesByUserId,
     assignRoleToUser,
+    revokeRoleFromUser,
 };

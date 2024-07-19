eda43b495ba32b2bc5195fd1de37b9f97d490160
Valeria Molina <valeriamolina123@gmail.com>
2024-07-19 11:10:14 -0400

    main page implemented

--- polish-finder-app/src/views/HomeView.vue
+++ polish-finder-app/src/views/HomeView.vue
@@ -3,8 +3,8 @@ import search from '../components/MainSearchBar.vue'
 </script>
 
 <template>
-  <div>
-    <div class="d-flex flex-column align-items-center justify-content-center min-vh-100">
+  <div class="d-flex flex-column align-items-center">
+    <div class="my-auto text-center">
       <h1>Find your perfect nail polish &#128133;</h1>
       <search></search>
     </div>

7e0c2879df94f7e904a2a4494bf115bc0fc6d00b
Valeria Molina <valeriamolina123@gmail.com>
2024-07-21 02:08:53 -0400

    initial brands page overview implemented

---
+++ polish-finder-app/src/components/BrandCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col">
+    <div class="card vw-20">
+      <a :href="website">
+        <img :src="logoUrl" class="card-img-top brand-logo-img" alt="Brand Logo" />
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">{{ brandName }}</h5>
+        <p class="card-text">
+          {{ description }}
+        </p>
+        <div class="row grid gap-0 column-gap-1">
+          <a :href="productsLink" class="col btn btn-primary">All products</a>
+          <a :href="addPolishLink" class="col btn btn-success">Add a polish</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps([
+  'brandName',
+  'logoUrl',
+  'description',
+  'productsLink',
+  'addPolishLink',
+  'website'
+])
+</script>
+
+<style scoped>
+.brand-logo-img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
--- polish-finder-app/src/views/BrandsView.vue
+++ polish-finder-app/src/views/BrandsView.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="container">
+  <div id="gradient-bg" class="d-flex flex-column align-items-center px-3 py-5">
     <h1>Brands</h1>
-    <p>Browse all our available brands. Can't find a brand? Let us know!</p>
-    <ul v-for="brand in brands.getBrands" :key="brand.brand_id">
-      <li>{{ brand.name }}</li>
-    </ul>
+    <p>
+      Browse all our available brands. Did we miss something?
+      <a href="/contribute/new/brand">Add it here!</a>
+    </p>
+    <div
+      class="white-bg container border shadow rounded row row-cols-1 row-cols-md-4 g-2 px-3 py-3"
+    >
+      <div v-for="brand in sampleBrands" :key="brand.brand_id">
+        <brandcard
+          :brandName="brand.brandName"
+          :logoUrl="brand.logoUrl"
+          :description="brand.description"
+          :productsLink="brand.productsLink"
+          :addPostLink="brand.addPostLink"
+          :website="brand.website"
+        ></brandcard>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,4 +27,73 @@ import { ref } from 'vue'
 import { useBrandStore } from '../stores/brands'
 const brands = useBrandStore()
 // brands.fetchAllBrands()
+
+import brandcard from '../components/BrandCard.vue'
+
+const sampleBrands = [
+  {
+    brand_id: 1,
+    brandName: 'Holo Taco',
+    logoUrl:
+      'https://yt3.googleusercontent.com/ytc/AIdro_nO9IAQvKjM7rQw4ay7axHuWe9UH4VQFvDUjz0_aQ48jg=s900-c-k-c0x00ffffff-no-rj',
+    description: 'Holo Taco is a nail polish brand.',
+    productsLink: '/',
+    addPostLink: '/',
+    website: 'https://holotaco.com'
+  },
+  {
+    brand_id: 2,
+    brandName: 'Holo Taco',
+    logoUrl:
+      'https://yt3.googleusercontent.com/ytc/AIdro_nO9IAQvKjM7rQw4ay7axHuWe9UH4VQFvDUjz0_aQ48jg=s900-c-k-c0x00ffffff-no-rj',
+    description: 'Holo Taco is a nail polish brand.',
+    productsLink: '/',
+    addPostLink: '/',
+    website: 'https://holotaco.com'
+  },
+  {
+    brand_id: 3,
+    brandName: 'Holo Taco',
+    logoUrl:
+      'https://yt3.googleusercontent.com/ytc/AIdro_nO9IAQvKjM7rQw4ay7axHuWe9UH4VQFvDUjz0_aQ48jg=s900-c-k-c0x00ffffff-no-rj',
+    description: 'Holo Taco is a nail polish brand.',
+    productsLink: '/',
+    addPostLink: '/',
+    website: 'https://holotaco.com'
+  },
+  {
+    brand_id: 4,
+    brandName: 'Holo Taco',
+    logoUrl:
+      'https://yt3.googleusercontent.com/ytc/AIdro_nO9IAQvKjM7rQw4ay7axHuWe9UH4VQFvDUjz0_aQ48jg=s900-c-k-c0x00ffffff-no-rj',
+    description: 'Holo Taco is a nail polish brand.',
+    productsLink: '/',
+    addPostLink: '/',
+    website: 'https://holotaco.com'
+  },
+  {
+    brand_id: 5,
+    brandName: 'Holo Taco',
+    logoUrl:
+      'https://yt3.googleusercontent.com/ytc/AIdro_nO9IAQvKjM7rQw4ay7axHuWe9UH4VQFvDUjz0_aQ48jg=s900-c-k-c0x00ffffff-no-rj',
+    description: 'Holo Taco is a nail polish brand.',
+    productsLink: '/',
+    addPostLink: '/',
+    website: 'https://holotaco.com'
+  }
+]
 </script>
+
+<style scoped>
+#gradient-bg {
+  background: linear-gradient(
+    110.5deg,
+    rgba(248, 196, 249, 0.66) 22.8%,
+    rgba(253, 122, 4, 0.15) 64.6%
+  );
+}
+
+.white-bg {
+  background-color: white;
+}
+</style>

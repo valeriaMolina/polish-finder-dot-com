1d0223e7f5e7d62d445fe38798f5f496356c76fc
Valeria Molina <valeriamolina123@gmail.com>
2024-09-03 00:57:53 -0400

    changed some stuff

--- polish-finder-app/vite.config.js
+++ polish-finder-app/vite.config.js
@@ -8,7 +8,7 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../dist'
+    outDir: 'dist'
   },
   plugins: [vue(), svgLoader()],
   resolve: {

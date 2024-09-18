c4a985414ac9228e2720acf28cdfe2e4e9b0f2c4
Valeria Molina <valeriamolina123@gmail.com>
2024-09-17 21:21:08 -0400

    implemented a basic polish card

---
+++ polish-finder-app/src/components/PolishCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col">
+    <div class="card vw-20 bg-color">
+      <img
+        src="https://res.cloudinary.com/dmmg4gidh/image/upload/t_no_img_available_squared/v1726597169/401px-No_picture_available_mkmndy.png"
+        class="card-img-top"
+        alt=""
+      />
+      <div class="card-body">
+        <div class="container px-0">
+          <div class="row align-items-start">
+            <div class="col">
+              <p class="fs-5 mb-0">Polish</p>
+            </div>
+            <div class="col">
+              <p class="text-end mb-0"><i class="bi bi-heart"></i></p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <p class="mb-0">Brand Name</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-color {
+  background-color: #ececec;
+}
+</style>
--- polish-finder-app/src/views/PolishesView.vue
+++ polish-finder-app/src/views/PolishesView.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="container">
-    <h1>Polishes</h1>
-    <p>Browse all our polishes</p>
-    <p>Did we miss one? Let us know!</p>
+  <div id="polishes" class="d-flex flex-column align-items-center px-3 py-5">
+    <div class="container mb-3">
+      <h1>Polishes</h1>
+      <p>Browse all our available nail polish</p>
+    </div>
+    <div
+      class="white-bg container border shadow rounded row row-cols-1 row-cols-md-5 g-2 px-3 py-3"
+    >
+      <PolishCard></PolishCard>
+      <PolishCard></PolishCard>
+      <PolishCard></PolishCard>
+      <PolishCard></PolishCard>
+      <PolishCard></PolishCard>
+    </div>
   </div>
 </template>
+
+<script setup>
+import PolishCard from '@/components/PolishCard.vue'
+</script>
+
+<style scoped>
+#polishes {
+  background-image: linear-gradient(120deg, #fccb90 0%, #d57eeb 100%);
+}
+
+.white-bg {
+  background-color: #f8f9fa;
+}
+</style>

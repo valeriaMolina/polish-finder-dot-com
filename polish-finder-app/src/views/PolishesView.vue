11d0b14a4a99f52997c5a5ef144e7a43ad744e1e
Valeria Molina <valeriamolina123@gmail.com>
2024-10-02 22:39:24 -0400

    created initial filter by sidebar

--- polish-finder-app/src/views/PolishesView.vue
+++ polish-finder-app/src/views/PolishesView.vue
@@ -1,22 +1,141 @@
 <template>
   <div id="polishes" class="d-flex flex-column py-5">
-    <div class="container mb-3">
-      <h1>Polishes</h1>
-      <p>Browse all our available nail polish</p>
-    </div>
     <div class="row">
       <div class="col-2 ms-2">
         <div class="white-bg container border shadow rounded row g-2 py-3">
-          <div class="">
-            <div>
-              <p>Brand</p>
+          <h4>Refine results</h4>
+          <div class="accordion accordion-flush" id="filterAccordion">
+            <div class="accordion-item">
+              <h4 class="accordion-header">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="false"
+                  aria-controls="collapseOne"
+                >
+                  Brand
+                </button>
+              </h4>
+              <div id="collapseOne" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                  <div class="nice-form-group">
+                    <input type="text" placeholder="Brand name" value="" />
+                  </div>
+                </div>
+              </div>
             </div>
-            <hr />
-            <div><p>Primary Color</p></div>
-            <hr />
-            <div><p>Secondary Colors</p></div>
-            <hr />
-            <div><p>Formula</p></div>
+            <div class="accordion-item">
+              <h4 class="acordion-header">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Color
+                </button>
+              </h4>
+              <div id="collapseTwo" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                  <fieldset>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="whiteColor" />
+                      <label for="whiteColor"> <span class="color-dot"></span> White</label>
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="greyColor" />
+                      <label for="greyColor">
+                        <span class="color-dot" style="background-color: grey"></span> Grey</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="blackColor" />
+                      <label for="blackColor"
+                        ><span class="color-dot" style="background-color: black"></span>
+                        Black</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="redColor" />
+                      <label for="redColor"
+                        ><span class="color-dot" style="background-color: red"></span> Red</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="orangeColor" />
+                      <label for="orangeColor"
+                        ><span class="color-dot" style="background-color: orange"></span>
+                        Orange</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="yellowColor" />
+                      <label for="yellowColor"
+                        ><span class="color-dot" style="background-color: yellow"></span>
+                        Yellow</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="greenColor" />
+                      <label for="greenColor"
+                        ><span class="color-dot" style="background-color: green"></span>
+                        Green</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="blueColor" />
+                      <label for="blueColor"
+                        ><span class="color-dot" style="background-color: blue"></span> Blue</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="indigoColor" />
+                      <label for="indigoColor"
+                        ><span class="color-dot" style="background-color: indigo"></span>
+                        Indigo</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="purpleColor" />
+                      <label for="purpleColor"
+                        ><span class="color-dot" style="background-color: purple"></span>
+                        Purple</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="pinkColor" />
+                      <label for="pinkColor"
+                        ><span class="color-dot" style="background-color: pink"></span> Pink</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="brownColor" />
+                      <label for="brownColor"
+                        ><span class="color-dot" style="background-color: brown"></span>
+                        Brown</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="goldColor" />
+                      <label for="goldColor"
+                        ><span class="color-dot" style="background-color: gold"></span> Gold</label
+                      >
+                    </div>
+                    <div class="nice-form-group">
+                      <input type="checkbox" id="silverColor" />
+                      <label for="silverColor"
+                        ><span class="color-dot" style="background-color: silver"></span>
+                        Silver</label
+                      >
+                    </div>
+                  </fieldset>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -102,5 +221,14 @@ onUnmounted(() => {
 
 .white-bg {
   background-color: #f8f9fa;
+}
+
+.color-dot {
+  height: 12px;
+  width: 12px;
+  background-color: white;
+  border-radius: 50%;
+  border: 1px solid black;
+  display: inline-block;
 }
 </style>

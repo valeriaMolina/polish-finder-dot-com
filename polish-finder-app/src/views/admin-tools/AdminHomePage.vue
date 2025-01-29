a50f52fafd96381d2ab5b4abdfd6c582b5cca45b
Valeria Molina <valeriamolina123@gmail.com>
2025-01-28 22:34:34 -0500

    finished admin tools for now

--- polish-finder-app/src/views/admin-tools/AdminHomePage.vue
+++ polish-finder-app/src/views/admin-tools/AdminHomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid">
         <AdminNav activeLink="admin"></AdminNav>
-        <div class="container">
+        <div class="border rounded px-3 py-3 mx-1 my-3">
             <h4>Dashboard</h4>
             <p>
                 Welcome to the admin dashboard. Here you can manage user feedback, role assignments,
